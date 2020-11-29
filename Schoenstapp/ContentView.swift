08d6c1f2881ecca60ef9d0d50e5cccb28df2dc7f
Kevin Schweitzer <kschweitzer@mobilecomputing.com.ar>
2020-11-29 11:51:57 -0300

    Initial Commit

---
+++ Schoenstapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Schoenstapp
+//
+//  Created by usuario on 29/11/2020.
+//  Copyright © 2020 Kevin Schweitzer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
